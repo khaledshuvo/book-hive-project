22c1948469adf883d3dabec7e915870483cfc0d3
Khaled <khaledssbd@gmail.com>
2024-03-28 02:08:00 +0600

    height adjusted for mobile

--- src/components/Wishlist.jsx
+++ src/components/Wishlist.jsx
@@ -6,7 +6,7 @@ const Wishlist = () => {
   const books = useContext(BookContext);
 
   return (
-    <div className="flex flex-col gap-6 min-h-full lg:min-h-[calc(100vh-545px)]">
+    <div className="flex flex-col gap-6 min-h-[calc(100vh-350px)] lg:min-h-[calc(100vh-545px)]">
       {books.map(book => (
         <WishedBook key={book.bookId} book={book}></WishedBook>
       ))}
