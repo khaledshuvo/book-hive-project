01d497ba2fabed658095677ad283b30cd0b2ee4c
Khaled <khaledssbd@gmail.com>
2024-03-28 01:55:53 +0600

    height adjusted

--- src/components/ReadBooks.jsx
+++ src/components/ReadBooks.jsx
@@ -6,7 +6,7 @@ const ReadBooks = () => {
   const books = useContext(BookContext);
 
   return (
-    <div className="flex flex-col gap-6">
+    <div className="flex flex-col gap-6 min-h-full lg:min-h-[calc(100vh-545px)]">
       {books.map(book => (
         <ReadBook key={book.bookId} book={book}></ReadBook>
       ))}
